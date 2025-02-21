96d055a2acf8cf47e5422e73a80564702f1af883
BrandonGriffith <BrandonGriffith92@gmail.com>
2025-02-20 20:01:28 -0500

    add: isAnagram and unittest

--- isAnagram.py
+++ isAnagram.py
@@ -1,3 +1,21 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        pass
+class IsAnagram:
+    def sorting(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        return sorted(s) == sorted(t)
+    
+    def hashing(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        hash = {}
+        for i in range(len(s)):
+            if s[i] in hash:
+                hash[s[i]] += 1
+            else:
+                hash[s[i]] = 1
+        for i in range(len(t)):
+            if t[i] in hash and hash[t[i]] > 0:
+                hash[t[i]] -= 1
+            else:
+                return False
+        return True
