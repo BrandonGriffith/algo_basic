3c9fe7ff145ea436ba99afcc2ded0566acdc4f15
BrandonGriffith <BrandonGriffith92@gmail.com>
2025-03-08 21:06:22 -0500

    add: tabulation def

--- fibonacci.py
+++ fibonacci.py
@@ -13,6 +13,14 @@ class Fibonacci:
             memo[i] = memo[i - 1] + memo[i - 2]
         return memo[x]
 
+    def tabulation(x : int) -> int:
+        if x <= 1:
+            return x
+        a, b = 0, 1
+        for _ in range(x-1):
+            a, b = b, a + b
+        return b
+
 def get_number():
     while True:
         number = input("Enter a number: ")
@@ -22,11 +30,13 @@ def get_number():
 
 def main():
     while True:
-        method = input("Choose method ('brute', 'memo' or 'exit'): ").strip().lower()
+        method = input("Choose method ('brute', 'memo', 'tab' or 'exit'): ").strip().lower()
         if method == 'brute':
             print(Fibonacci.brute_force(get_number()))
         elif method == 'memo':
             print(Fibonacci.memoization(get_number()))
+        elif method == 'tab':
+            print(Fibonacci.tabulation(get_number()))
         elif method == 'exit':
             break
         else:
